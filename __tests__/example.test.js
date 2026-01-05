a0c2f0d0bbf7f0454586f008f818d99a7129ae91
AlenaRudenko <pineapple001@mail.ru>
2026-01-05 10:01:51 +0300

    fix: фикс тестов

--- __tests__/example.test.js
+++ __tests__/example.test.js
@@ -1,3 +1,9 @@
-test("true is true", () => {
-  expect(true).toBe(true);
-});
+import { strict as assert } from 'node:assert';
+try {
+  assert.strictEqual(true, true, "true должно быть true");
+  console.log("Test approved")
+} catch(error) {
+  console.error("Test failed",error);
+  process.exit(1);
+}
+console.log('Все тесты завершены.');
